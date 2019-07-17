729bc62ff3e265621dbf1e7e9c53bf8823947478
crazyjgg7 <1378150147@qq.com>
2019-07-17 14:18:51 +0800

    test
    2019firstpush

---
+++ flaskday1.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+
+from functools import reduce 
+app=Flask(__name__)
+
+@app.route('/')     #url修饰器
+def hello() -> str:
+    return 'hello world from Flask'
+
+app.run()
+
+@app.route('/hi')
+def readfile(filename):
+    with open(filename,'r',encoding='utf-8')as files:
+        content = reduce(lambda x,y:x+y,files.readlines())
+        return content
+    return 'content'   
